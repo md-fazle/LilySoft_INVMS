adcb21bae0030df83a78ac2d65e93e99745e759e
md-fazle <rf.fazle.rabbi@gmail.com>
2025-08-27 15:57:55 +0600

    Add project files.

---
+++ LilySoft_INVMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LilySoft_INVMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
