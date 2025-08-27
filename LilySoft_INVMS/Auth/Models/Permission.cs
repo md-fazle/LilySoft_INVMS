f133715eaf36d5e8d303b5d96669150b83714705
md-fazle <rf.fazle.rabbi@gmail.com>
2025-08-27 19:28:54 +0600

    Auth Models

---
+++ LilySoft_INVMS/Auth/Models/Permission.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LilySoft_INVMS.Auth.Models
+{
+    public class Permission
+    {
+        [Key]
+        public int PermissionId { get; set; }
+
+        [Required, StringLength(150)]
+        public string? PermissionName { get; set; }
+
+        [StringLength(255)]
+        public string? Description { get; set; }
+
+        // Navigation
+        public virtual ICollection<RolePermission>? RolePermissions { get; set; }
+
+    }
+}
--- LilySoft_INVMS/Auth/Models/Users.cs
+++ LilySoft_INVMS/Auth/Models/Users.cs
@@ -1,11 +1,27 @@
-﻿namespace LilySoft_INVMS.Auth.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LilySoft_INVMS.Auth.Models
 {
     public class Users
     {
-       
-        
-       
-       
+        [Key]
+        public int userId { get; set; }
 
+        public string? userName { get; set; } 
+
+        public string? password { get; set; }
+
+        [Required, StringLength(150)]
+        public string? email { get; set; }
+
+        [Required, StringLength(15)]
+        public string? phoneNumber { get; set; }    
+            
+        public bool isActive { get; set; }  
+
+
+        public int RoleId { get; set; }
+
+        public Roles ? Role { get; set; } 
     }
 }
