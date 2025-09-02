b30bf69765df26601670f6e5b00a83f6460f8f22
md-fazle <rf.fazle.rabbi@gmail.com>
2025-09-02 12:46:33 +0600

    role-back everything

--- LilySoft_INVMS/Program.cs
+++ LilySoft_INVMS/Program.cs
@@ -1,5 +1,3 @@
-using LilySoft_INVMS.Auth.Services;
-using LilySoft_INVMS.DBContext;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
@@ -11,25 +9,10 @@ builder.Services.AddControllersWithViews();
 // DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("DefaultConnection not found.");
-builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
-
-// AuthServices
-builder.Services.AddScoped<AuthServices>();
-
-// Cookie auth
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Auth/Login";
-        options.AccessDeniedPath = "/Auth/AccessDenied";
-        options.ExpireTimeSpan = TimeSpan.FromHours(1);
-        options.SlidingExpiration = true;
-    });
-
+ 
 // Authorization
 builder.Services.AddAuthorization();
-builder.Services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
-builder.Services.AddScoped<IAuthorizationHandler, PermissionAuthorizationHandler>();
+ 
 
 var app = builder.Build();
 
