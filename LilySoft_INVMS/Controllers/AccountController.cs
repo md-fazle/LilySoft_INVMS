5206014d19cab0a3ba439eaf97e8ef167488dd00
md-fazle <rf.fazle.rabbi@gmail.com>
2025-09-07 11:53:20 +0600

    bug fix in Account controller

--- LilySoft_INVMS/Controllers/AccountController.cs
+++ LilySoft_INVMS/Controllers/AccountController.cs
@@ -7,35 +7,40 @@ namespace LilySoft_INVMS.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly SignInManager<Users>signInManager;
+        private readonly SignInManager<Users> signInManager;
         private readonly UserManager<Users> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
 
-        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager, RoleManager<IdentityRole> roleManager)
+        public AccountController(
+            SignInManager<Users> signInManager,
+            UserManager<Users> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
             this.roleManager = roleManager;
         }
 
+        // ===== Login =====
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginViewModels models)
+        public async Task<IActionResult> Login(LoginViewModels model)
         {
             if (!ModelState.IsValid)
             {
-                return View(models);
+                return View(model);
             }
 
             var result = await signInManager.PasswordSignInAsync(
-                models.Email,
-                models.password,
-                models.rememberMe,
+                model.Email!,         // null-forgiving since [Required]
+                model.password!,      // same here
+                model.rememberMe,
                 lockoutOnFailure: false);
 
             if (result.Succeeded)
@@ -44,10 +49,12 @@ namespace LilySoft_INVMS.Controllers
             }
 
             ModelState.AddModelError(string.Empty, "Invalid login attempt");
-            return View(models);
+            return View(model);
         }
+
+        // ===== Register =====
         [HttpGet]
-        public async Task<IActionResult> Register()
+        public IActionResult Register()
         {
             return View();
         }
@@ -56,35 +63,39 @@ namespace LilySoft_INVMS.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegistrationViewModels model)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(model);
             }
+
             var user = new Users
             {
-                UserName = model.Email,
-                Email = model.Email,
-                FullName = model.Name
+                UserName = model.Email!,
+                Email = model.Email!,
+                FullName = model.Name!
             };
 
-            var result = await userManager.CreateAsync(user, model.Password);
-            if(result.Succeeded)
+            var result = await userManager.CreateAsync(user, model.Password!);
+
+            if (result.Succeeded)
             {
                 var roleExists = await roleManager.RoleExistsAsync("Viewer");
                 if (!roleExists)
                 {
                     await roleManager.CreateAsync(new IdentityRole("Viewer"));
                 }
+
                 await userManager.AddToRoleAsync(user, "Viewer");
                 await signInManager.SignInAsync(user, isPersistent: false);
+
                 return RedirectToAction("Login", "Account");
- 
             }
 
-            foreach(var error in result.Errors)
+            foreach (var error in result.Errors)
             {
                 ModelState.AddModelError(string.Empty, error.Description);
             }
+
             return View(model);
         }
     }
