64eb6b36d85642174d0d1abab55c97ac33d0a4ff
md-fazle <rf.fazle.rabbi@gmail.com>
2025-09-08 23:50:51 +0600

    fontend

---
+++ LilySoft_INVMS/wwwroot/js/sidebar.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const toggleBtn = document.getElementById("toggle-btn");
+
+    toggleBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("collapsed");
+    });
+});
+
